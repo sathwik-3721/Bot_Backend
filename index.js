425e5482bed84c31a28b58f92c5e72981e53e800
Sathwik Ramisetty <sathwik3721@gmail.com>
2024-10-29 15:19:02 +0530

    Updared code

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ app.post('/get-response', async(req, res) => {
     }  
 });
 
+// test api
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Hello'});
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
